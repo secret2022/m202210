442c09a1f8d417e365cd30557bbb5104f2727254
sojeong-il <playmetaverse@gmail.com>
2022-12-21 20:41:11 +0900

    Initial commit

---
+++ src/meta1025/test.java
@@ -0,0 +1,20 @@
+package meta1025;
+
+public class test {
+
+	public static void main(String[] args) {
+		System.out.println("테스트");
+
+		String st = Integer.toString(100);
+		int git	= Integer.parseInt(st);
+		Integer oit = Integer.valueOf(git);
+	
+		System.out.println(oit);
+	
+		System.out.println("합산결과 값: "	+ oit);
+	
+	}
+
+}
+
+
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author sojeong-il
+ *
+ */
+module m202210 {
+}
